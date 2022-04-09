bb25a21bf5a4cb7d0e09715dc9248b27d97ec88e
iamadoctorforreal <52762924+iamadoctorforreal@users.noreply.github.com>
2022-04-09 17:58:50 +0100

    Update Balance.js

--- packages/react-app/src/Components/Balance.js
+++ packages/react-app/src/Components/Balance.js
@@ -34,38 +34,44 @@ const Balance = () => {
 
     }
 
+  const checkEthBalance = async () => {
+        let balance = await window.ethereum.request({ method: 'eth_getBalance',
+            params: [
+                accounts[0]
+            ]
+    }).catch((err) => {
+        console.log(err);
+    });
+    
+    
+    balance = parseInt(balance);
+    balance = balance / Math.pow(10, 18);
+    console.log(balance);
+    document.getElementById("balance").innerHTML =checkEthBalance();
+    }
+
    /* async function connectWallet() {
-
         if(typeof window.ethereum !== 'undefined') {
-
             await requestAccount();
 
            // const provider = new ethers.providers.web3Provider(window.ethereum);
-
           
-} */ 
-
+        } */ 
+    
     
 
-    
-    return (
 
+  return (
     <div>
-
     <header className='flex-auto  font-semibold text-slate-900'>
-
-        <button className=' h-10  px-6 font-semibold rounded-md bg-indigo-900 text-white ' onClick={requestAccount}>
-
+        <button className=' h-10  px-6 font-semibold rounded-md bg-indigo-900 text-white ' onClick={requestAccount }>
             Connect Wallet</button>
-
         <h3>Wallet Address:   {walletAddress}</h3>
 
+        <h3 id='balance'> Balance: </h3>
     </header>
-
     </div>
-
   )
-
   }
 
 export default Balance
